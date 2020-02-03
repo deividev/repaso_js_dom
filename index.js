5fb86534995e9a97e8d772a5d59217514cdd42f5
Alejandro Garrido <alejandro.garrido@boream.com>
2020-02-03 14:18:19 +0100

    Update Ex

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+function pintar() {
+
+}
+
+function addToDo() {
+
+}
+
+function addDeleteBtnElement() {
+
+}
+
+function addEditBtnElement() {
+
+}
+
+function addDoneBtnElement() {
+
+}
+
+function addDoneBtnElement() {
+
+}
+
+function doneElement(e) {
+
+}
+
+function editElement(e) {
+
+}
+
+function saveElement(e) {
+
+}
+
+function addTodoOnEnter(e) {
+
+}
